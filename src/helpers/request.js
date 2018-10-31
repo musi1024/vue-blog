248ad14bcb9d2c21498a232a9248f53093e9a76c
musi <957028613@qq.com>
2018-11-01 01:39:40 +0800

    封装数据接口

---
+++ src/helpers/request.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import {
+  Message
+} from 'element-ui'
+
+axios.defaults.baseURL = 'http://blog-server.hunger-valley.com'
+axios.withCredentials = true
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+
+export default function request(url, type = 'GET', data = {}) {
+  return new Promise((resolve, reject) => {
+    let option = {
+      url,
+      method: type
+    }
+    if (type.toLowerCase() === 'get') {
+      option.params = data
+    } else {
+      option.data = data
+    }
+    axios(option).then((respons) => {
+      if (respons.data.status === 'ok') {
+        resolve(respons.data)
+        console.log(respons.data)
+      } else {
+        Message.error(respons.data.msg)
+        reject(respons.data)
+      }
+    }).catch((error) => {
+      Message.error('网络异常')
+      reject({
+        msg: '网络异常'
+      })
+    })
+  })
+}

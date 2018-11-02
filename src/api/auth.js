3755d9565370899666b461f32a614204e62f10e6
musi <957028613@qq.com>
2018-11-02 23:00:55 +0800

    封装 auth api

---
+++ src/api/auth.js
@@ -0,0 +1,23 @@
+import request from '@/helpers/request'
+
+const Url = {
+    Register: '/auth/register',
+    Login: '/auth/login',
+    Logout: '/auth/logout',
+    GetInfo: '/auth',
+}
+
+export default {
+    register({username, password}) {
+        return request(Url.Register, 'POST', {username, password})
+    },
+    login({username, password}) {
+        return request(Url.Login, 'POST', {username, password})
+    },
+    logout() {
+        return request(Url.Logout)
+    },  
+    getinfo() {
+        return request(Url.GetInfo)
+    }
+}

b30720c9e24962f5d85f340be6b414a36f2f2b4a
musi <957028613@qq.com>
2018-11-06 22:04:00 +0800

    完成 detail 页面样式

--- src/pages/Detail/template.vue
+++ src/pages/Detail/template.vue
@@ -1,7 +1,20 @@
 <template>
-    <div id="detail">
-        详情页面
-    </div>
+  <div id="detail">
+    <section class="user-info"> 
+      <router-link to="/user" class="avatar">
+          <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="">
+      </router-link>  
+      <h3>前端异步大揭秘</h3>
+      <p>
+        <router-link to="/user">若愚</router-link>  发布于3天前
+      </p>
+    </section>
+    <section class="article">
+        
+    </section>
+  </div>
 </template>
+
 <script src="./template.js"></script>
-<style src="./template.css"></style>
+
+<style src="./template.less" lang="less"></style>

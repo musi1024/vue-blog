4aaf2615446fe5af20d118fbc89221174f16457d
musi <957028613@qq.com>
2018-11-06 21:08:23 +0800

    header & footer 组件基本样式

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,3 +3,15 @@
     <p>© musi1024 2018 wechat: yauck12345</p>
   </footer>
 </template>
+
+<style scoped>
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+
+</style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -24,4 +24,56 @@
       }
     }
   }
+
 </script>
+
+<style lang="less">
+  @import "../assets/base.less";
+
+  header.no-login {
+    padding: 0 12% 30px 12%;
+    background: @bgColor;
+    display: grid;
+    justify-items: center;
+    h1 {
+      color: #fff;
+      font-size: 40px;
+      margin: 60px 0 0 0;
+      text-transform: uppercase;
+    }
+    p {
+      margin: 15px 0 0 0;
+      color: #fff;
+    }
+    .btns {
+      margin-top: 20px;
+    }
+    button {
+      margin: 20px 5px 0;
+    }
+  }
+  header.login {
+    display: flex;
+    align-items: center;
+    background: @bgColor;
+    h1 {
+      margin: 0;
+      padding: 0;
+      color: #fff;
+      font-size: 40px;
+      text-transform: uppercase;
+      flex: 1;
+    }
+    .edit {
+      color: #fff;
+      font-size: 30px;
+    }
+    .avatar {
+      width: 40px;
+      height: 40px;
+      border: 1px solid #fff;
+      border-radius: 50%;
+      margin-left: 15px;
+    }
+  }
+</style>

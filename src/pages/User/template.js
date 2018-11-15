fd4f168f0f26ba2e7d16e2008165feb7ab5e0fa2
musi <957028613@qq.com>
2018-11-16 00:45:34 +0800

    完善用户博客页面

--- src/pages/User/template.js
+++ src/pages/User/template.js
@@ -1,8 +1,36 @@
+import blog from '@/api/blog'
+
 export default {
   name: 'User',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      blogs: [],
+      total: 0,
+      page: 1,
+      user: {}
+    }
+  },
+  created() {
+    this.userId = this.$route.params.userId
+    this.page = parseInt(this.$route.query.page) || 1
+    blog.getByUserId(this.userId, { page: this.page }).then((res) => {
+      console.log(res)
+      this.blogs = res.data
+      this.total = res.total
+      this.page = res.page
+      if(res.data.length > 0) {
+        this.user = res.data[0].user
+      }
+    })
+  },
+  methods: {
+    onPageChange(newPage) {
+      blog.getByUserId(this.userId, { page: newPage }).then((res) => {
+        this.blogs = res.data
+        this.total = res.total
+        this.page = res.page
+        this.$router.push({ path: `/user/${this.userId}`, query: { page: newPage}})  
+      })
     }
   }
 }

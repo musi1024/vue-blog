3da46651383da1f4d9dd412c0b03963a9af043b5
musi <957028613@qq.com>
2018-11-18 20:50:35 +0800

    增加create edit 校验

--- src/pages/Create/template.js
+++ src/pages/Create/template.js
@@ -12,11 +12,26 @@ export default {
   },
   methods: {
     onCreate() {
-      blog.create({ title: this.title, content: this.content, description: this.description, atIndex: this.atIndex})
-        .then((res) => {
-          this.$message.success(res.msg)
-          this.$router.push({ path: `/detail/${res.data.id}`})
-        })
+      if (this.title === '') {
+        this.$message.error('博客标题不能为空')
+      } else if (this.content === '') {
+        this.$message.error('博客内容不能为空')
+      } else {
+        blog.create({
+            title: this.title,
+            content: this.content,
+            description: this.description,
+            atIndex: this.atIndex
+          })
+          .then((res) => {
+            this.$message.success(res.msg)
+            this.$router.push({
+              path: `/detail/${res.data.id}`
+            })
+          })
+      }
+
     }
-  }
+  },
+
 }
--- src/pages/Edit/template.js
+++ src/pages/Edit/template.js
@@ -13,20 +13,37 @@ export default {
   },
   created() {
     this.blogId = this.$route.params.blogId
-    blog.getDetail({ blogId: this.blogId }).then(res => {
+    blog.getDetail({
+      blogId: this.blogId
+    }).then(res => {
       this.title = res.data.title
       this.description = res.data.description
       this.content = res.data.content
       this.atIndex = res.data.atIndex
-    })  
+    })
   },
   methods: {
     onEdit() {
-      blog.update({blogId: this.blogId}, { title: this.title, content: this.content, description: this.description, atIndex: this.atIndex})
-        .then((res) => {
-          this.$message.success(res.msg)
-          this.$router.push({ path: `/detail/${res.data.id}`})
-        })
+      if (this.title === '') {
+        this.$message.error('博客标题不能为空')
+      } else if (this.content === '') {
+        this.$message.error('博客内容不能为空')
+      } else {
+        blog.update({
+            blogId: this.blogId
+          }, {
+            title: this.title,
+            content: this.content,
+            description: this.description,
+            atIndex: this.atIndex
+          })
+          .then((res) => {
+            this.$message.success(res.msg)
+            this.$router.push({
+              path: `/detail/${res.data.id}`
+            })
+          })
+      }
     }
   }
 }
